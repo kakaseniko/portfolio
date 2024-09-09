0f92739578013dbc334b08a1d94289003f429359
kakas <kakas.eniko@gmail.com>
2024-09-09 11:59:32 +0200

    home page styling, profile card

--- Home.py
+++ Home.py
@@ -13,23 +13,14 @@ def main():
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
     st.write(f"""
-             <h1 style="text-align:center;">Welcome! I'm Eniko Kakas.</h1>
+             <h1 style="text-align:center; margin-bottom:1rem; margin-left:1rem;">Welcome! I'm Eniko Kakas.</h1>
              """, unsafe_allow_html=True)
 
 
     # Profile image
     with open("./images/pic.jpg", "rb") as img_file:
         img = "data:image/jpg;base64," + base64.b64encode(img_file.read()).decode()
-
-    st.write(f"""
-             <div class="profile">
-                <img class="bd" src="{img}" alt="Eniko Kakas">
-                <h2>Software & AI Engineer 👩‍💻</h2>
-            </div>
-        """, 
-        unsafe_allow_html=True)
     
-    # Social Icons
     social_icons = {
         # Platform: [URL, Icon]
         "LinkedIn": ["https://www.linkedin.com/in/eniko-kakas/", "https://cdn-icons-png.flaticon.com/512/174/174857.png"],
@@ -38,13 +29,31 @@ def main():
     }
 
     social_icons_html = [f"<a href='{social_icons[platform][0]}' target='_blank' style='margin-right: 10px;'><img class='social-icon' src='{social_icons[platform][1]}'' alt='{platform}''></a>" for platform in social_icons]
-    st.write(f"""
-        <div style="display: flex; justify-content: center; margin-bottom: 20px;">
-            {''.join(social_icons_html)}
-        </div>""", 
-    unsafe_allow_html=True)
 
-    st.title("About me")
+    st.write(f"""
+             <div class="profile">
+                <div class="profile-frame">
+                    <div class="circle">
+                        <div class="img-frame">
+                            <img class="bd" src="{img}" alt="Eniko Kakas">
+                        </div>
+                    </div>
+                    <h2 style="margin-left: 1rem;">Software & AI Engineer 👩‍💻</h2>
+                    <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+                    {''.join(social_icons_html)}
+                    </div>
+                </div>
+            </div>
+        """, 
+        unsafe_allow_html=True)
+
+    st.write(f"""
+             <div class="section-title">
+                <h1 style="padding:0;">About me</h1>
+             </div>
+             """,
+             unsafe_allow_html=True)
+
     st.write("I am a Software engineer...")
     with open("./images/Resume.pdf", "rb") as pdf_file:
         pdf_bytes = pdf_file.read()
@@ -58,18 +67,13 @@ def main():
     st.subheader("Jack of all trades")
     st.write("software, recreation manager, setter, dancer, a whole lot of hobbies")
 
-
-
-
     st.write("Ask the chatbot if you want to know more...")
     st.write("Check out my projects...")
 
-    #with open("./images/Resume.pdf","rb") as f:
-    #    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    #    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
-    #    st.markdown(pdf_display, unsafe_allow_html=True)
     st.write(f"""
-             <h1 style="text-align:end;">Contact me</h1>
+             <div class="section-title" id="contact" style="position:absolute; right: 0;">
+                <h1 style="padding: 0; text-align: end;">Contact me</h1>
+             </div>
              """, unsafe_allow_html=True)
 
 if __name__ == '__main__':
