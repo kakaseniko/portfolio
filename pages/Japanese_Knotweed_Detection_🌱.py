8d6585950ebf2c17daff070ce827c87febd21bec
kakas <kakas.eniko@gmail.com>
2024-09-30 12:05:24 +0200

    laravel project

--- pages/Japanese_Knotweed_Detection_🌱.py
+++ pages/Japanese_Knotweed_Detection_🌱.py
@@ -6,15 +6,15 @@ st.header("Detect Japanese Knotweed along the roads 🌱")
 
 
 st.write("""This project was the one I graduated with from Fontys with cum laude 🥲. It was provided by and executed for <a href="https://www.ecogoggle.nl/" target="_blank">EcoGoggle</a>, 
-         a company that works with technology to monitor nature. The goal was to prove that street view images can be used to identify plants (in this case the Japanese knotweed)
+         a company that works with technology to monitor nature. The goal was to prove that street view images can be used to identify plants (in this case Japanese knotweed)
          automatically with computer vision.""", unsafe_allow_html=True)
-st.image("./images/4.png", caption="Japanese Knotweed along the road.", use_column_width=True)
+st.image("./images/4.png", caption="Japanese Knotweed along the road", use_column_width=True)
 st.write("""I especially enjoyed working on this project because the domain/ cause is close to my heart, I really like and value nature and I find it important to 
          take action to protect it and to improve its current state. It was fulfilling to apply my knowledge for a cause that I believe in. I feel like this was the 
          perfect assignment that marks the end of my studies. </br></br>
          The process included data labeling (about 10.000 images was enough to condition myself to spot Japanese knotweed everywhere I go 🫠), testing different model 
          tasks (classification, object detection, and segmentation), and building a prototype application.""", unsafe_allow_html=True)
-st.image("./images/5.png", caption="Prototype to display Japanese knotweed locations.", use_column_width=True)
+st.image("./images/5.png", caption="Prototype to display Japanese knotweed locations", use_column_width=True)
 st.write("""The image above shows the final outcome, a prototype that can be used to analyze a whole folder, find the images with Japanese knotweed, and display their 
          location on a map. The marked dots on the map are clickable and they show the image on which the knotweed was detected. </br></br>
          Since the project was a success, the company is planning to continue it and extend the model to be able to recognize multiple plant species and 
--- pages/Laravel_es_magico_🦄.py
+++ pages/Laravel_es_magico_🦄.py
@@ -2,7 +2,34 @@ import streamlit as st
 st.set_page_config(page_title="Laravel es magico", page_icon="🦄", layout="centered")
 st.header("Create anything with Laravel 🦄")
 
+st.write("""...because Laravel is magical! 🪄 This sentence summarizes my internship on Gran Canaria 🏝️ where I spent a half year as a full-stack developer.</br></br>
+         During the ICT study program at Fontys students are required to do two internships, one at the beginning of the third year and one during graduation. Fontys also
+          encourages students to go abroad for a semester if they can. I thought I would seize the opportunity and go for an internship abroad. The plan: find a company at a 
+         destination where I can surf 🏄‍♀️. (Cause why would it be enough to learn programming? I needed to pick up a new hobby too...). </br></br>
+         This idea kicked off a new adventure for me. I was super lucky and found a company called Calima Solutions on Gran Canaria that offered Digital Opportunities internships for
+          Erasmus students. I applied and two months later I was working in an office with an ocean view. </br></br> 
+         """, unsafe_allow_html=True)
+st.image("./images/6.jpg", caption="Office at Calima Solutions", width=400)
+
+st.write("""My job was to implement functionalities in a full-stack application made for an upcoming health resort. This project was the one that thought me about Angular, 
+         Ionic, Laravel, Filament, and Tailwind. Next to that, it also thought me soft skills as I was part of the development team, participated in daily stand ups, 
+         code reviews, and of course some unforgettable team outings. The picture below gives a small impression about the application I was working on, but it does not 
+         cover the whole system.""", unsafe_allow_html=True)
+st.image("./images/3.png", caption="Admin panel made with Filament", use_column_width=True)
+st.write("""
+         The project was big and many developers worked on it for a while by the time I got there, consequently I had to work with legacy code and spend some time on 
+         bug fixes and code refactoring. This gave me an opportunity to learn to work with different code quality tools (like Enlightn and PHPStan) and of course to 
+         complain a lot to my supervisor 😜. 
+         </br></br>
+         At the end, all the efforts paid off and I left the company with a system that was easier to maintain, performed better, had less security vulnerabilities, and 
+         had a documentation available for future developers.
+         </br></br>
+         The semester ended with a successful project and tearful goodbyes.
+         Looking back I realized that that half year did not only extended my IT knowledge but also thought me how to adapt to a new living situation, it allowed me to explore a different culture, and gave me 
+         the opportunity to learn a little bit of Spanish, lots of surfing, and make new friends for life. It was an entire adventure which made me grow as a person and 
+         for which I will be grateful for my whole life.""", unsafe_allow_html=True)
+st.image("./images/7.jpg", caption="Christmas presentation at Calima Solutions", use_column_width=True)
+st.image("./images/8.jpg", caption="Team outing on a catamaran", use_column_width=True)
+st.image("./images/9.JPG", caption="Surfing in Gran Canaria", use_column_width=True)
 
 
-st.write("...because Laravel is magical! 🪄, Gran Canaria, working with Laravel, Filament, Ionic, Angular, IQure project")
-st.image("./images/3.png", caption="Working with Laravel, Filament, Ionic, Angular, IQure project", use_column_width=True)
