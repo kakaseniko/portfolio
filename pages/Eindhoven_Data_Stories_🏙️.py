84af96a5c3071b2cfb331f6456fcc8909d3cfca8
kakas <kakas.eniko@gmail.com>
2024-10-10 08:43:31 +0200

    testing import

--- pages/Eindhoven_Data_Stories_🏙️.py
+++ pages/Eindhoven_Data_Stories_🏙️.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objects as go
 
 df = pd.read_csv('./data/trendy_eindhoven_data_Eniko.csv')
